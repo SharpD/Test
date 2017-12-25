883e95cf4f8aa678ef3bdb270921d6ec772ab928
SharpD <34827073+SharpD@users.noreply.github.com>
2017-12-25 08:16:28 +0300

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string res;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                res = "";
+
+                res += (i % 3 == 0 ? "Fizz" : "");
+                
+                res += (i % 5 == 0 ? "Buzz" : "");
+
+                res += (String.IsNullOrWhiteSpace(res) ? i.ToString() : "");
+
+                Console.Write(res + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
